508e7525b6744683782e7f40fd41b7a452e994a1
Wayne Graham <wgraham@clir.org>
2017-11-10 09:35:05 -0500

    Update to headers

---
+++ header-template/menu.inc.php
@@ -0,0 +1,57 @@
+<style>.top-bar{display:block;}</style>
+<div class="top-bar">
+  <div class="<?php echo $header_container; ?>">
+      <div class="top-bar-left">
+        <nav class="top-bar-menu clir-custom-menu">
+          <ul class="list-inline">
+            <li class="menu-item menu-item-type-custom menu-item-object-custom">
+              <a href="https://staging.clir.org">
+                <!-- <i class="icon-clir-logo-square anchor-color bigger"></i> -->
+                <span class="clir-font">CLIR</span>
+              </a>
+            </li>
+              <li class="hidden-xs menu-item menu-item-type-custom menu-item-object-custom">
+                <a href="https://staging.clir.org/fellowships"><span>Fellowships &amp; Grants</span></a>
+              </li>
+              <li class="menu-item menu-item-type-custom menu-item-object-custom">
+                <a href="https://staging.diglib.org/"><span>DLF</span></a>
+              </li>
+              <li class="hidden-xs menu-item menu-item-type-custom menu-item-object-custom">
+                <a href="https://staging.clir.org/pubs"><span>Publications</span></a>
+              </li>
+              <li class="hidden-xs menu-item menu-item-type-custom menu-item-object-custom">
+                <a href="https://staging.clir.org/join">Join</a>
+              </li>
+              <li class="menu-item menu-item-type-custom menu-item-object-custom">
+                 <a class="give" href="https://staging.clir.org/give">Give
+                    <i class="fa fa-heart"></i>
+                </a>
+              </li>
+            </ul>
+          </nav>
+      </div>
+      <div class="top-bar-right">
+        <ul class="list-inline hidden-xs">
+          <li class="dropdown high-z">
+            <a href="#" class="dropdown-tottle" data-toggle="dropdown">
+              <i class="icon-clir-logo header-logo-size bigger"></i>
+              CLIR Programs
+              <i class="fa fa-angle-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-right clir-programs-menu" aria-labelled-by="clir-programs" role="menu">
+                <li><a href="https://www.diglib.org/">Digital Library Federation</a></li>
+                <li><a href="https://www.diglib.org/forum/">DLF Forum</a></li>
+                <li><a href="http://dlme.clir.org">Digital Library of the Middle East</a></li>
+                <li><a href="https://www.clir.org/fellowships/hiddencollections">Digitizing Hidden Special Collections and Archives</a></li>
+                <li><a href="https://www.clir.org/recordings-at-risk">Recordings at Risk</a></li>
+                <li><a href="https://www.clir.org/fellowships/mellon">Mellon Fellowships for Dissertation Research</a></li>
+                <li><a href="http://www.leadingchangeinstitute.org">Leading Change Institute</a></li>
+                <li><a href="https://www.diglib.org/groups/e-research-network/">DLF eResearch Network</a></li>
+                <li><a href="https://www.clir.org/fellowships/postdoc">Postdoctoral Fellowship Program</a></li>
+              </ul>
+            </li>
+          </ul>
+      </div>
+  </div>
+  <div class="clear"></div>
+</div><!-- .top_bar -->
