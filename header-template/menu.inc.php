22f2792e6159b3837937adb7026154fb20c0f6f5
Wayne Graham <wgraham@clir.org>
2017-11-29 09:56:28 -0500

    Fix header links
    Update header links to point to correct domain.

--- header-template/menu.inc.php
+++ header-template/menu.inc.php
@@ -5,25 +5,25 @@
         <nav class="top-bar-menu clir-custom-menu">
           <ul class="list-inline">
             <li class="menu-item menu-item-type-custom menu-item-object-custom">
-              <a href="https://staging.clir.org">
+              <a href="https://www.clir.org">
                 <!-- <i class="icon-clir-logo-square anchor-color bigger"></i> -->
                 <span class="clir-font">CLIR</span>
               </a>
             </li>
               <li class="hidden-xs menu-item menu-item-type-custom menu-item-object-custom">
-                <a href="https://staging.clir.org/fellowships"><span>Fellowships &amp; Grants</span></a>
+                <a href="https://www.clir.org/fellowships"><span>Fellowships &amp; Grants</span></a>
               </li>
               <li class="menu-item menu-item-type-custom menu-item-object-custom">
-                <a href="https://staging.diglib.org/"><span>DLF</span></a>
+                <a href="https://www.diglib.org/"><span>DLF</span></a>
               </li>
               <li class="hidden-xs menu-item menu-item-type-custom menu-item-object-custom">
-                <a href="https://staging.clir.org/pubs"><span>Publications</span></a>
+                <a href="https://www.clir.org/pubs"><span>Publications</span></a>
               </li>
               <li class="hidden-xs menu-item menu-item-type-custom menu-item-object-custom">
-                <a href="https://staging.clir.org/about/become-a-sponsor-or-member/">Join</a>
+                <a href="https://www.clir.org/about/become-a-sponsor-or-member/">Join</a>
               </li>
               <li class="menu-item menu-item-type-custom menu-item-object-custom">
-                 <a class="give" href="https://staging.clir.org/give">Give
+                 <a class="give" href="https://www.clir.org/give">Give
                     <i class="fa fa-heart"></i>
                 </a>
               </li>
